d0a47bf5bb7b2010d1fb368becf8d9bb1b5fd70b
hs-truealtitude <null>
2021-10-13 08:43:05 +0000

    CSS Fixes & Changes: Notification Icon on Cohort Card, Application Modal etc Update:

--- widgetsComputed/mentorCountProgrammesInvolvedIn.js
+++ widgetsComputed/mentorCountProgrammesInvolvedIn.js
@@ -1,7 +1,7 @@
 return function  () {
 let count = 0
 
-count = $getGrid('programmeData').filter(row => row.mentors == $getUser('profileID')).length 
+    count = $getGrid('programmeData').filter(row => row.mentors == $getUser('profileID')).length 
 
 return count
 }
